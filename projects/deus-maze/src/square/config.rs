8c2b0c5f9973d837c773a8b847271b6125f12449
Aster <aster@vers.site>
2013-01-06 04:47:15 +0000

    🐤 Add config for 2d dungeon

--- projects/deus-maze/src/square/config.rs
+++ projects/deus-maze/src/square/config.rs
@@ -1,4 +1,3 @@
-
 use super::*;
 
 
@@ -10,6 +9,21 @@ impl Default for Maze2DConfig {
             height: 10,
             entry: (1, 0),
             exit: (9, 8),
+            seed,
+        }
+    }
+}
+
+impl Default for Dungeon2DConfig {
+    fn default() -> Self {
+        let seed = rand::random::<[u8; 32]>();
+        Self {
+            width: 10,
+            height: 10,
+            entry: (1, 0),
+            exit: (9, 8),
+            room_width: 3..5,
+            room_height: 3..5,
             seed,
         }
     }
@@ -33,5 +47,19 @@ impl Maze2DConfig {
 
     pub fn rng(&self) -> rand::rngs::StdRng {
         SmallRng::from_seed(self.seed).into()
+    }
+}
+
+impl Maze2DConfig {
+    pub fn as_dungeon(&self) -> Dungeon2DConfig {
+        Dungeon2DConfig {
+            width: self.width,
+            height: self.height,
+            entry: self.entry,
+            exit: self.exit,
+            room_width: 3..5,
+            room_height: 3..5,
+            seed: self.seed,
+        }
     }
 }
--- projects/deus-maze/src/square/mod.rs
+++ projects/deus-maze/src/square/mod.rs
@@ -1,9 +1,11 @@
+use std::ops::Range;
 use ndarray::Array2;
 use rand::rngs::SmallRng;
 use rand::SeedableRng;
 
 mod config;
-mod build;
+mod build_path;
+mod build_dungeon;
 
 pub struct Maze2DConfig {
     pub width: usize,
@@ -12,6 +14,17 @@ pub struct Maze2DConfig {
     pub exit: (usize, usize),
     pub seed: [u8; 32],
 }
+
+pub struct Dungeon2DConfig {
+    pub width: usize,
+    pub height: usize,
+    pub entry: (usize, usize),
+    pub exit: (usize, usize),
+    pub room_width: Range<usize>,
+    pub room_height: Range<usize>,
+    pub seed: [u8; 32],
+}
+
 
 pub struct Maze2D {
     config: Maze2DConfig,
